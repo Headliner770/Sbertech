339a21287d617fc76d7c0e01b455d0ad25d930af
Headliner770 <paul248022@gmail.com>
2025-01-15 21:01:21 +0300

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import "./App.css";
 import BlockCompany from "./components/BlockCompany";
 import BlockTeam from "./components/BlockTeam";
 import BlockPlatformV from "./components/BlockPlatformV";
+import stLogo from "./img/Logo.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -49,7 +50,8 @@ function App() {
         </section> */}
 
         <div className="column-tree">
-          3 Продукты
+          <div>3 Продукты</div>
+
           <div className="column-tree-title">
             <h3 className="column-one-title-text">
               Наши продукты входят в Реестр российского ПО
@@ -81,6 +83,42 @@ function App() {
           </div>
         </div>
         <div className="column-one"></div>
+        <div>
+          <a href="">
+            Подробнее
+            <div></div>
+          </a>
+        </div>
+
+        <div className="blocks-main">
+          <div className="flex-block">
+            <div className="block-blue">
+              <div>
+                <h4 className="block-blue-title">Образовательные стримы</h4>
+                <p className="block-blue-text">
+                  Стажировки и проекты Сбера, чтобы быстро влиться в команду
+                </p>
+                <a href="#" className="blocks-details">
+                  Подробнее
+                </a>
+              </div>
+            </div>
+            <div className="block-green">
+              <div>
+                <h4 className="block-green-title">
+                  Отличные условия для каждого
+                </h4>
+                <p className="block-green-text">
+                  ДМС с первого дня, снижение ипотечной ставки до 4%, скидки на
+                  спорт и образование
+                </p>
+                <a href="#" className="blocks-details">
+                  Подробнее
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="column-one">
           4 Культура
@@ -100,8 +138,84 @@ function App() {
           </div>
         </div>
         <div className="column-one"></div>
-        <div class="rounded-b-10 border-x border-b h-[14px] custom-border"></div>
       </main>
+
+      <footer>
+        <div className="footer-main">
+          <div className="footer-logo-column">
+            <img src={stLogo} className="stLogo" alt="Лого" />
+          </div>
+          <div className="footer-link-block">
+            <div className="footer-company-column">
+              <div>
+                <a href="">О компании</a>
+              </div>
+              <div>
+                <a href="">Команда</a>
+              </div>
+              <div>
+                <a href="">Условия работы</a>
+              </div>
+              <div>
+                <a href="">Блог</a>
+              </div>
+              <div>
+                <a href="">Программы в вузах</a>
+              </div>
+              <div>
+                <a href="">МФТИ</a>
+              </div>
+              <div>
+                <a href="">Контакты</a>
+              </div>
+              <div>
+                <a href="">Карта сайта</a>
+              </div>
+            </div>
+            <div className="footer-vacancies-column">
+              <div>
+                <a href="">Вакансии</a>
+              </div>
+              <div>
+                <a href="">Реестр программного обеспечения</a>
+              </div>
+              <div>
+                <a href="">Пользовательское соглашение</a>
+              </div>
+              <div>
+                <a href="">Политика конфиденциальности</a>
+              </div>
+              <div>
+                <a href="">Политика обработки и защиты персональных данных</a>
+              </div>
+              <div>
+                <a href="">Условия распространения персональных данных</a>
+              </div>
+              <div>
+                <a href="">Согласие на обработку персональных данных</a>
+              </div>
+              <div>
+                <a href="">Юридическая информация</a>
+              </div>
+              <div>
+                <a href="">Горячая линия комплаенс</a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-info-column">
+            <div>
+              <a href="">info@sbertech.ru</a>
+            </div>
+            <div>
+              <a href=""></a>+7 495 669 08 08
+            </div>
+            <div>
+              "Контакты для СМИ"<br></br>
+              <a href="">PR@sbertech.ru</a>
+            </div>
+          </div>
+        </div>
+      </footer>
 
       <div>
         <a href="https://vite.dev" target="_blank">
